7a8b6dc3c64ffb8416e75b639d0b66555f831a86
Victor Alves dos Santos <victorasan@gmail.com>
2021-09-08 21:17:53 -0300

    Get da API Filmes com async

--- FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -15,10 +15,16 @@ namespace FilmesAPI.Controllers
         private static List<Filme> filmes = new List<Filme>();
         
         [HttpPost]
-        public void AdicionaFilme(Filme filme)
+        public void AdicionaFilme([FromBody] Filme filme)
         {
             filmes.Add(filme);
             Console.WriteLine(filme.Titulo);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Filme>>> GetFilmes()
+        {
+            return filmes.ToList();
         }
     }
 }
